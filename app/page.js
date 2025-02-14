36f55e6b7bd5c1b7b2b7f92f328a0ffa674d2ff7
Julius Cinco Cesar <knznsmn@icloud.com>
2025-02-14 22:20:11 +0400

    Add new fonts

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <h1 className={styles.title}>knznsmn</h1>
-      <p className={styles.description}>Graphic Design</p>
-      <p className={styles.description}>Web Development</p>
-
+      <p className={styles.description}>
+        <Link href="/notebook" aria-label="Go to notebook">
+          online notebook
+        </Link>
+      </p>
     </main>  
   );
 }
