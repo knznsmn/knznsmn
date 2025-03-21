9c2207223d58c9de37cb66f11803e251483d8d90
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-03-21 10:24:06 +0400

    Deploy static site

---
+++ _next/static/chunks/app/about/page-500ccf4a24135660.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{8866:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,5613,23))},5613:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[870,441,517,358],()=>s(8866)),_N_E=e.O()}]);
