a6506c16dd8376e5fbba9d493e498b73a9a4c910
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-03-23 08:35:57 +0400

    Restructure doc directory

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Feuilles',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Second Brain on the cloud!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -26,7 +26,7 @@ const config = {
   projectName: 'knznsmn', // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -86,10 +86,9 @@ const config = {
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
-            position: 'left',
+            position: 'right',
             label: 'Docs',
           },
-          {to: '/notes', label: 'Notes', position: 'left'},
           {to: '/blog', label: 'Blog', position: 'right'},
           {
             href: 'https://github.com/knznsmn/knznsmn',
